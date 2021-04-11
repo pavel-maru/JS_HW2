a3df69fbd7b1c3df633492d294ff8e57d046656a
pavel-maru <71653912+pavel-maru@users.noreply.github.com>
2021-04-11 08:57:00 +0300

    second commit

--- task_2_4.js
+++ task_2_4.js
@@ -7,51 +7,36 @@ let a = 3;
 
 switch (a) {
     case 0:
-        alert('0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15');
-        break;
+        alert(0);
     case 1:
-        alert('1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15');
-        break;
+        alert(1);
     case 2:
-        alert('2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15');
-        break;
+        alert(2);
     case 3:
-        alert('3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15');
-        break;
+        alert(3);
     case 4:
-        alert('4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15');
-        break;
+        alert(4);
     case 5:
-        alert('5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15');
-        break;
+        alert(5);
     case 6:
-        alert('6, 7, 8, 9, 10, 11, 12, 13, 14, 15');
-        break;
+        alert(6);
     case 7:
-        alert('7, 8, 9, 10, 11, 12, 13, 14, 15');
-        break;
+        alert(7);
     case 8:
-        alert('8, 9, 10, 11, 12, 13, 14, 15');
-        break;
+        alert(8);
     case 9:
-        alert('9, 10, 11, 12, 13, 14, 15');
-        break;
+        alert(9);
     case 10:
-        alert('10, 11, 12, 13, 14, 15');
-        break;
+        alert(10);
     case 11:
-        alert('11, 12, 13, 14, 15');
-        break;
+        alert(11);
     case 12:
-        alert('12, 13, 14, 15');
-        break;
+        alert(12);
     case 13:
-        alert('13, 14, 15');
-        break;
+        alert(13);
     case 14:
-        alert('14, 15');
-        break;
+        alert(14);
     case 15:
-        alert('15');
-        break;
+        alert(15);
 }
+
